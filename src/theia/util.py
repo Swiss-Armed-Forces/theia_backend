fc09f9badff0ca8c1b82cff942051e1a506b710f
Jürg Huber <>
2026-02-03 11:21:01 +0100

    Refactoring: Calculate marcum q function in a separate module; add unit tests

--- src/theia/util.py
+++ src/theia/util.py
@@ -1,4 +1,5 @@
 import numpy as np
+from scipy import integrate, special
 
 
 def erp_to_power(erp: float, losses: float, gain: float) -> float:
@@ -17,7 +18,7 @@ def erp_to_power(erp: float, losses: float, gain: float) -> float:
         Losses [dB]
     gain: float
         Gain [dBi]
-    
+
     Returns
     -------
     power: float
@@ -30,5 +31,42 @@ def erp_to_power(erp: float, losses: float, gain: float) -> float:
 def to_dB(value: float) -> float:
     return 10 * np.log10(value)
 
+
 def from_dB(value: float) -> float:
-    return 10**(value / 10)
+    return 10 ** (value / 10)
+
+
+def marcum_q_integrand(v: float, alpha: float) -> float:
+    return v * np.exp(-(v * v + alpha * alpha) / 2) * special.iv(0, alpha * v)
+
+
+def marcum_q_function(alpha: float, beta: float) -> float:
+    """
+    Calculate the values of the marcum q function.
+
+    Parameters
+    ----------
+    alpha: float
+        alpha parameter; must be <= 30
+    beta: float
+        beta parameter
+    
+    Returns
+    -------
+    float
+
+    Raises
+    ------
+    ValueError
+        If alpha is > 30 due to numerical instability of the Bessel function
+    """
+    if alpha > 30.0:
+        raise ValueError("alpha must be <= 30 to avoid overflow in Bessel functions!")
+    return (
+        1
+        - integrate.quad(
+            lambda x: marcum_q_integrand(x, alpha),
+            0,
+            beta,
+        )[0]
+    )
