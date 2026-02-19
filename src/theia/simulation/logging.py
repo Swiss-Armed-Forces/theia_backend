c0fff19f0ac57c8f2e2e99405084a9e9c7ac2b61
Jürg Huber <>
2026-02-19 11:32:06 +0100

    Feature: Add loader for simulation logs

--- src/theia/simulation/logging.py
+++ src/theia/simulation/logging.py
@@ -1,7 +1,14 @@
 import abc
+import itertools
 import json
 
-from theia.types import ActiveRadarDetection, SituationalPicture, Snapshot
+import pandas as pd
+from stonesoup.models.measurement.base import MeasurementModel
+from stonesoup.types.detection import Detection
+from stonesoup.types.groundtruth import GroundTruthPath, GroundTruthState
+
+from theia.coordinates import CoordinateTransformations
+from theia.types import ActiveRadarDetection, Radar, Receiver, SituationalPicture, Snapshot, Transmitter
 
 
 class AbstractSimulationLogger(abc.ABC):
@@ -149,3 +156,147 @@ class InMemoryLogger(AbstractSimulationLogger):
 
     def end(self):
         pass
+
+
+class LogLoader:
+    """Load JSON file written by FileLogger."""
+    def __init__(self, path: str, measurement_model: MeasurementModel):
+        self._measurement_model = measurement_model
+        with open(path, "r") as file:
+            self._data = json.load(file)
+        self._load_snapshots()
+        self._load_monostatic_radars(is_blue=True)
+        self._load_target_ground_truth(is_blue=False)
+        self._load_detections(is_blue=True)
+
+    @property
+    def blue_monostatic_radars(self) -> list[Radar]:
+        return self._blue_monostatic_radars
+
+    @property
+    def blue_monostatic_radar_detections(self) -> list[Detection]:
+        return self._blue_monostatic_radar_detections
+
+    @property
+    def red_target_ground_truth(self) -> dict[int, GroundTruthPath]:
+        return self._red_target_ground_truth
+
+    def _load_snapshots(self):
+        self._snapshots = [Snapshot.model_validate(d) for d in self._data["snapshots"]]
+
+    def _load_monostatic_radars(self, is_blue: bool):
+        if not is_blue:
+            raise NotImplementedError()
+
+        blue_transmitters: dict[int, Transmitter] = {}
+        blue_receivers: dict[int, Receiver] = {}
+        for snapshot in self._snapshots:
+            blue_transmitters.update({tx.id: tx for tx in snapshot.blue_transmitters})
+            blue_receivers.update({rx.id: rx for rx in snapshot.blue_receivers})
+
+        self._blue_monostatic_radars = []
+        for tx, rx in itertools.product(
+            blue_transmitters.values(), blue_receivers.values()
+        ):
+            if tx.point == rx.point:
+                self._blue_monostatic_radars.append(Radar(transmitter=tx, receiver=rx))
+
+    def _load_target_ground_truth(self, is_blue: bool):
+        if is_blue:
+            raise NotImplementedError()
+
+        red_target_states = []
+        for snapshot in self._snapshots:
+            for target in snapshot.red_targets:
+                x, y, z = CoordinateTransformations.geodetic_to_cartesian(
+                    *target.point.as_tuple()
+                )
+                red_target_states.append(
+                    {
+                        "id": target.id,
+                        "time": snapshot.time,
+                        "x": x,
+                        "y": y,
+                        "z": z,
+                        "vx": target.velocity.vx,
+                        "vy": target.velocity.vy,
+                        "vz": target.velocity.vz,
+                    }
+                )
+        red_target_states = pd.DataFrame(red_target_states).sort_values(["id", "time"])
+
+        self._red_target_ground_truth: dict[int, GroundTruthPath] = {}
+        for target_id, target_rows in red_target_states.groupby("id"):
+            ground_truth_path = GroundTruthPath()
+            for _, row in target_rows.iterrows():
+                ground_truth_path.append(
+                    GroundTruthState(
+                        row[["x", "vx", "y", "vy", "z", "vz"]].values,
+                        timestamp=row["time"],
+                        metadata={"target_id": target_id},
+                    )
+                )
+            self._red_target_ground_truth[target_id] = ground_truth_path
+
+    def _load_detections(self, is_blue: bool):
+        if not is_blue:
+            raise NotImplementedError()
+
+        blue_detections = list(
+            itertools.chain.from_iterable(
+                [
+                    det["active_radar_detections"]
+                    for det in self._data["active_radar_detections"]
+                    if det["team"] == "blue"
+                ]
+            )
+        )
+        blue_detections = [
+            ActiveRadarDetection.model_validate(d) for d in blue_detections
+        ]
+        properties = []
+        for detection in blue_detections:
+            x, y, z = CoordinateTransformations.geodetic_to_cartesian(
+                *detection.target.point.as_tuple()
+            )
+            properties.append(
+                {
+                    "id": detection.detection_id,
+                    "radar_id": detection.radar.receiver.id,
+                    "target_id": detection.target.id,
+                    "time": detection.time,
+                    "range": detection.target_range,
+                    "elevation": detection.elevation_angle,
+                    "azimuth": detection.azimuth_angle,
+                    "target_x": x,
+                    "target_y": y,
+                    "target_z": z,
+                    "target_vx": detection.target.velocity.vx,
+                    "target_vy": detection.target.velocity.vy,
+                    "target_vz": detection.target.velocity.vz,
+                }
+            )
+
+        df_blue_detections = pd.DataFrame(properties).sort_values(
+            ["time", "radar_id", "target_id"]
+        )
+
+        self._blue_monostatic_radar_detections: list[Detection] = []
+        for _, row in df_blue_detections.iterrows():
+            self._blue_monostatic_radar_detections.append(
+                Detection(
+                    state_vector=row[
+                        [
+                            "target_x",
+                            "target_y",
+                            "target_z",
+                        ]
+                    ],
+                    timestamp=row["time"],
+                    metadata={
+                        "radar_id": row["radar_id"],
+                        "target_id": row["target_id"],
+                    },
+                    measurement_model=self._measurement_model,
+                )
+            )
