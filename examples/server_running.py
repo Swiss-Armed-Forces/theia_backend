53b0ffbde471b220f75e792710c32e6105ebdfc4
Jürg Huber <>
2026-03-05 06:23:11 +0100

    Feature: Add server that exposes the simulation and an experiment script to run it

---
+++ examples/server_running.py
@@ -0,0 +1,123 @@
+import datetime
+import functools
+from pathlib import Path
+import threading
+
+import uvicorn
+
+from theia.data_loading import load_trajectory_file
+from theia.simulation.controllers.controller_group import ControllerGroup
+from theia.simulation.controllers.monostatic_radar_controller import (
+    MonostaticRadarController,
+)
+from theia.simulation.controllers.waypoint_target_controller import (
+    WaypointTargetController,
+)
+from theia.simulation.logging import SituationalPictureBuffer
+from theia.simulation.server import create_app
+from theia.simulation.simulator import (
+    Simulator,
+    TimeCriterion,
+    run_simulation_until_completion,
+)
+from theia.simulation.tracking import MonostaticSingleSensorTracker
+from theia.types import Point, Polarization, Radar, Receiver, Transmitter
+
+print("Initialise...")
+
+buffer = SituationalPictureBuffer()
+
+################################################
+# Setup the simulation.
+################################################
+
+FREQUENCY = 3_000  # Hz
+POWER = 500_000  # W
+DIAMETER = 4.0  # m
+BANDWIDTH = 5  # MHz
+
+radar = Radar(
+    transmitter=Transmitter(
+        id=0,
+        point=Point(lat=47.349491, lon=8.492063, alt=856.2037851199802),
+        power=POWER,
+        erp=1000.0,
+        antenna_height=10.0,
+        antenna_diameter=DIAMETER,
+        frequency=FREQUENCY,
+        pulse_width=1.0,
+        polarization=Polarization.VERTICAL,
+        bandwidth=BANDWIDTH,
+        max_coherent_integration_time=0.5,
+        antenna_efficiency_value=0.6,
+        vertical_attenuation=None,
+        horizontal_attenuation=None,
+    ),
+    receiver=Receiver(
+        id=0,
+        point=Point(lat=47.349491, lon=8.492063, alt=856.2037851199802),
+        antenna_height=10.0,
+        diameter=DIAMETER,
+        cpi_pulses=1.0,
+        pfa=1e-06,
+        min_elevation=-20.0,
+        max_elevation=60.0,
+        rotation_time=10.0,
+        bandwidth=BANDWIDTH,
+        gain=0,
+        losses=0,
+        noise_temperature=300.0,
+        noise_figure=1.9,
+        antenna_efficiency_value=0.6,
+        vertical_attenuation=None,
+        horizontal_attenuation=None,
+    ),
+)
+
+# Load trajectories from OpenSky.
+trajectories, _ = load_trajectory_file(
+    f"{Path(__file__).resolve().parent}/../data/data_opensky_2022-06-27.csv"
+)
+
+scripted_target_controller = ControllerGroup(
+    [WaypointTargetController.from_trajectory(t) for t in trajectories]
+)
+
+# Build the simulator.
+start_time = min([t.times[0] for t in trajectories])
+stop_time = max([t.times[-1] for t in trajectories])
+
+time_step = datetime.timedelta(seconds=1)
+
+simulator = Simulator(
+    blue_controller=MonostaticRadarController(radar),
+    red_controller=scripted_target_controller,
+    blue_tracker=MonostaticSingleSensorTracker(),
+    start_time=start_time,
+    time_step=time_step,
+    min_time_per_step=datetime.timedelta(seconds=0),
+    termination_criterion=TimeCriterion(stop_time),
+    seed=4054080,
+    listener=buffer,
+)
+
+################################################
+# Setup the server.
+################################################
+app = create_app(buffer)
+
+################################################
+# Start the simulation and the server.
+################################################
+sim_thread = threading.Thread(
+    target=functools.partial(
+        run_simulation_until_completion,
+        simulator=simulator,
+    ),
+    daemon=False,
+)
+sim_thread.start()
+
+print("Start simulation...")
+
+uvicorn.run(app, host="127.0.0.1", port=8000)
---
+++ src/theia/simulation/server.py
@@ -0,0 +1,118 @@
+"""Provide a fastapi server for exposing the latest simulation state to external consumers."""
+
+import datetime
+from enum import Enum
+
+from fastapi import FastAPI
+import numpy as np
+import pydantic
+
+from theia.coordinates import CoordinateTransformations
+from theia.simulation.logging import SituationalPictureBuffer
+from theia.types import Radar
+
+
+class Team(Enum):
+    blue = "BLUE"
+    red = "RED"
+
+
+class TrackPoint(pydantic.BaseModel):
+    time: datetime.datetime
+    """Time coordinate of the trajectory."""
+    lat: float
+    """Latitude [decimal °]"""
+    lon: float
+    """Longitude [decimal °]"""
+    alt: float
+    """Altitude [m above sea level]"""
+    v_east: float
+    """Velocity along east direction in local ENU frame [m / s]"""
+    v_north: float
+    """Velocity along north direction in local ENU frame [m / s]"""
+    v_up: float
+    """Velocity along up direction in local ENU frame [m / s]"""
+
+
+class ExtrapolatedTrack(pydantic.BaseModel):
+    points: list[TrackPoint]
+
+
+class ExtrapolatedSituationalPicture(pydantic.BaseModel):
+    time: datetime.datetime
+    friendly_radars: list[Radar]
+    enemy_tracks: list[ExtrapolatedTrack]
+
+
+class GroundTruth(pydantic.BaseModel):
+    pass
+
+
+# def extrapolate_track(track: Track):
+
+
+def create_app(
+    buffer: SituationalPictureBuffer,
+    max_extrapolation_time: datetime.timedelta = datetime.timedelta(minutes=1),
+    extrapolation_resolution: datetime.timedelta = datetime.timedelta(seconds=1),
+) -> FastAPI:
+    app = FastAPI()
+
+    @app.get("/time")
+    def get_time() -> datetime.datetime:
+        # We assume that the red and blue situational pictures share the same time.
+        # This is consistent with the implementation of the main simulation loop.
+        # The differences would be negligible, anyway.
+        return buffer.get_situational_picture(True).time
+
+    @app.get("/situational_picture/{which}")
+    def get_situational_picture(which: Team) -> ExtrapolatedSituationalPicture:
+        picture = buffer.get_situational_picture(which == Team.blue)
+
+        extrapolated_tracks: list[ExtrapolatedTrack] = []
+        print("N enemies: ", len(picture.enemy_targets))
+        for track in picture.enemy_targets:
+            times = track._times
+            states = np.vsplit(track._y, track._y.shape[0])
+            print(states)
+
+            # t0 = datetime.datetime.fromtimestamp(
+            #     times[-1],
+            #     tz=datetime.timezone.utc,
+            # )
+            # t = t0
+            # while t < t0 + max_extrapolation_time:
+            #     times.append(t)
+            #     states.append(track(t))
+            #     t += extrapolation_resolution
+
+            states = [s.flatten() for s in states]
+
+            track_points = []
+            for t, y in zip(times, states, strict=True):
+                x, vx, y, vy, z, vz = y
+                lat, lon, alt = CoordinateTransformations.cartesian_to_geodetic(x, y, z)
+                # p = Point(lat=lat, lon=lon, alt=alt)
+                # v_east, v_north, v_up = CoordinateTransformations.ecef_to_enu(
+                #     p,
+                #     (vx, vy, vz),
+                # )
+                track_points.append(
+                    TrackPoint(
+                        time=t,
+                        lat=lat,
+                        lon=lon,
+                        alt=alt,
+                        v_east=0.0,
+                        v_north=0.0,
+                        v_up=0.0,
+                    )
+                )
+            extrapolated_tracks.append(ExtrapolatedTrack(points=track_points))
+        return ExtrapolatedSituationalPicture(
+            time=picture.time,
+            friendly_radars=picture.friendly_radars,
+            enemy_tracks=extrapolated_tracks,
+        )
+
+    return app
