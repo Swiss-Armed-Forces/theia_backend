901d822e49984206d2ed48005d37ee2cbce87746
Jürg Huber <>
2026-03-27 09:43:30 +0100

    Refactoring: Add Uetliberg radar to test data

--- src/theia/test_data.py
+++ src/theia/test_data.py
@@ -96,3 +96,71 @@ class TestSituationLoader:
             transmitter_labels={0: "Tx"},
             target_labels={0: "Target (southbound)"},
         )
+
+
+def get_uetliberg_radar(
+    frequency: float = 3_000,
+    power: float = 500_000,
+    diameter: float = 4.0,
+    bandwidth: float = 5.0,
+):
+    """
+    Parameters
+    ----------
+    frequency: float, default 3000.0
+        Frequency [Hz]
+    power: float, default 500000.0
+        Power [W]
+    diameter: float, default 4.0
+        Diameter [m]
+    bandwidth: float, default 5.0
+        Bandwidth [MHz]
+    """
+
+    radar_lat = POSITIONS_OF_INTEREST["Uetliberg"]["lat"]
+    radar_lon = POSITIONS_OF_INTEREST["Uetliberg"]["lon"]
+    radar_alt = POSITIONS_OF_INTEREST["Uetliberg"]["alt"]
+
+    return Radar(
+        transmitter=Transmitter(
+            id=0,
+            point=Point(lat=radar_lat, lon=radar_lon, alt=radar_alt),
+            power=power,
+            erp=1000.0,
+            antenna_height=10.0,
+            antenna_diameter=diameter,
+            frequency=frequency,
+            pulse_width=1.0,
+            polarization=Polarization.VERTICAL,
+            bandwidth=bandwidth,
+            max_coherent_integration_time=0.5,
+            antenna_efficiency_value=0.6,
+            vertical_attenuation=None,
+            horizontal_attenuation=None,
+        ),
+        receiver=Receiver(
+            id=0,
+            point=Point(lat=radar_lat, lon=radar_lon, alt=radar_alt),
+            antenna_height=10.0,
+            diameter=diameter,
+            cpi_pulses=1.0,
+            pfa=1e-06,
+            min_elevation=-20.0,
+            max_elevation=60.0,
+            rotation_time=10.0,
+            bandwidth=bandwidth,
+            gain=0,
+            losses=0,
+            noise_temperature=300.0,
+            noise_figure=1.9,
+            antenna_efficiency_value=0.6,
+            vertical_attenuation=None,
+            horizontal_attenuation=None,
+        ),
+        error_model=MonostaticRadarMeasurementModel(
+            min_range_uncertainty=0.0,
+            max_range_uncertainty=0.0,
+            min_angular_uncertainty=0.0,
+            max_angular_uncertainty=0.0,
+        ),
+    )
