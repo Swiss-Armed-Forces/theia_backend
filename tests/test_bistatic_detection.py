f14a7efc9a490b4aca82db65fc7c31b31d7c3309
Renato Bellotti <184365954+RenatoBellottiCH@users.noreply.github.com>
2026-04-10 06:50:28 +0200

    Code cleanup: Remove obsolete testing code

--- tests/test_bistatic_detection.py
+++ tests/test_bistatic_detection.py
@@ -1,40 +1,5 @@
 import unittest
 
-# from theia.data_loading_testing import load_pcl_reference_data
-# from theia.detection.pcl import calculate_pcl_detection
-# from theia.types import ConstantRcsModel
-
-
-# class BistaticDetectionTest(unittest.TestCase):
-#     def test_against_reference(self):
-#         trajectories, detections = load_pcl_reference_data()
-#         for detection_ref, snr in detections:
-#             # print("ID = ", detection_ref.detection_id)
-#             bistatic_range_ref = detection_ref.bistatic_range
-#             bistatic_doppler_ref = detection_ref.doppler_shift
-#             detection = calculate_bistatic_detection(
-#                 detection_ref.receiver,
-#                 detection_ref.transmitter,
-#                 detection_ref.target,
-#                 rcs_model=ConstantRcsModel(detection_ref.target.cross_section),
-#             )
-#             # snr_calc = calculate_snr(
-#             #     detection_ref.transmitter,
-#             #     detection_ref.receiver,
-#             #     detection_ref.target.point,
-
-#             # )
-
-#             self.assertIsNot(detection, None)
-
-#             self.assertAlmostEqual(bistatic_range_ref, detection.bistatic_range)
-#             self.assertAlmostEqual(
-#                 bistatic_doppler_ref,
-#                 detection.doppler_shift,
-#                 delta=0.2,
-#             )
-#             # self.assertAlmostEqual(snr, snr_calc)
-
 
 if __name__ == "__main__":
     unittest.main()
