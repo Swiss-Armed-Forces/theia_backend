0dfceb017fe1c0f0bbba279d75b7cea9f1334b59
Renato Bellotti <184365954+RenatoBellottiCH@users.noreply.github.com>
2026-05-04 15:29:10 +0200

    Feature: Add optional transmitter to target

--- src/theia/simulation/controllers/waypoint_target_controller.py
+++ src/theia/simulation/controllers/waypoint_target_controller.py
@@ -1,13 +1,15 @@
 from __future__ import annotations
 import datetime
+from typing import Optional
 
 import numpy as np
 from scipy.interpolate import CubicSpline
 from theia.coordinates import CoordinateTransformations
 from theia.types import (
     Controller,
+    MonostaticSensor,
+    PclSensor,
     Point,
-    AbstractSensor,
     RcsModel,
     SituationalPicture,
     Target,
@@ -23,7 +25,24 @@ class WaypointTargetController(Controller):
         times: list[datetime.datetime],
         waypoints: list[Point],
         rcs_model: RcsModel,
+        radar: Optional[MonostaticSensor] = None,
     ):
+        """
+        Parameters
+        ----------
+        target_id: int
+            Unique identifier
+        times: list[datetime.datetime]
+            Times at which the waypoints are reached
+        waypoints: list[Point]
+            Waypoints of the trajectory
+        rcs_model: RcsModel
+            Radar cross section (RCS) model
+        radar: Optional[MonostaticSensor], default None
+            On-board radar (if any); is always active if present
+            The position of the radar is updated to the target's current position,
+            so the "point" property of the transmitter and receiver are ignored
+        """
         assert len(times) == len(waypoints)
         self._target_id = target_id
         self._rcs_model = rcs_model
@@ -37,19 +56,29 @@ class WaypointTargetController(Controller):
         )
         self._f = CubicSpline(times, positions_xyz, extrapolate=False)
         self._v = self._f.derivative()
+        self._sensor = radar
 
     def get_monostatic_radars(
         self,
         situational_picture: SituationalPicture,
         dt: datetime.timedelta,
-    ) -> list[AbstractSensor]:
-        return []
+    ) -> list[MonostaticSensor]:
+        if self._sensor is None:
+            return []
+
+        t = (situational_picture.time + dt).timestamp()
+        x, y, z = self._f(t)
+        lat, lon, alt = CoordinateTransformations.cartesian_to_geodetic(x, y, z)
+        p = Point(lat=lat, lon=lon, alt=alt)
+        self._sensor.transmitter.point = p
+        self._sensor.receiver.point = p
+        return [self._sensor]
 
     def get_pcl_sensors(
         self,
         situational_picture: SituationalPicture,
         dt: datetime.timedelta,
-    ) -> list[AbstractSensor]:
+    ) -> list[PclSensor]:
         return []
 
     def get_targets(
@@ -65,17 +94,26 @@ class WaypointTargetController(Controller):
             # Out-of-bounds time.
             return []
         else:
+            tx = None
+            if self._sensor is not None:
+                tx = self._sensor.transmitter.model_copy()
+                p = Point(lat=lat, lon=lon, alt=alt)
+                tx.point = p
             return [
                 Target(
                     id=self._target_id,
                     point=Point(lat=lat, lon=lon, alt=alt),
                     cross_section_model=self._rcs_model,
                     velocity=Velocity(vx=v_xyz[0], vy=v_xyz[1], vz=v_xyz[2]),
+                    transmitter=tx,
                 )
             ]
 
     @staticmethod
-    def from_trajectory(trajectory: Trajectory) -> WaypointTargetController:
+    def from_trajectory(
+        trajectory: Trajectory,
+        sensor: Optional[MonostaticSensor] = None,
+    ) -> WaypointTargetController:
         points: list[Point] = []
         for lat, lon, alt in zip(
             trajectory.lats, trajectory.lons, trajectory.alts, strict=True
