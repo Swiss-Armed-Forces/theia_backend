56a2e7146e9e87698609dde8837f474a3b652050
Renato Bellotti <184365954+RenatoBellottiCH@users.noreply.github.com>
2026-06-04 16:03:59 +0200

    Update terrain creation script

--- create_terrain.py
+++ create_terrain.py
@@ -1,5 +1,5 @@
 from theia.terrain_fast_los import build_terrain_tree
 
 
-tree = build_terrain_tree(46, 49, 7, 9, subsample_stride=2)
-tree.save("tree_lat46:49_lon7:9.zip")
+tree = build_terrain_tree(45, 49, 6, 11, subsample_stride=2)
+tree.save("tree_lat45:49_lon6:11.zip")
