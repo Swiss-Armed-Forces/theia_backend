33241136bb2437a068c969dd7ea8cf2ee76ac533
Renato Bellotti <184365954+RenatoBellottiCH@users.noreply.github.com>
2026-06-30 07:32:41 +0200

    Refactoring: Effectors fire at Targets

--- src/theia/effectors.py
+++ src/theia/effectors.py
@@ -1,16 +1,14 @@
 from dataclasses import dataclass
 
 from theia.config import UNKNOWN_ID, UNKNOWN_TIME
-from theia.coordinates import CoordinateTransformations
 from theia.distance import line_of_sight_distance
 from theia.terrain import AbstractTerrainModel
 from theia.types import (
     AbstractEffector,
     DirectShot,
     IndirectShot,
-    Point,
+    Target,
     TheiaException,
-    Track,
 )
 
 
@@ -31,7 +29,7 @@ class NoLosException(TheiaException):
 class DirectFireEffector(AbstractEffector):
     terrain: AbstractTerrainModel
 
-    def fire(self, track: Track) -> DirectShot:
+    def fire(self, target: Target) -> DirectShot:
         """
         Raises
         ------
@@ -42,26 +40,22 @@ class DirectFireEffector(AbstractEffector):
         NoLosException:
             There is no line-of-sight to the target
         """
-        x, y, z, vx, vy, vz = track._y[-1]
-        lat, lon, alt = CoordinateTransformations.cartesian_to_geodetic(x, y, z)
-        track_point = Point(lat=lat, lon=lon, alt=alt)
-
         d = line_of_sight_distance(
             self.point.lat,
             self.point.lon,
             self.point.alt,
-            lat,
-            lon,
-            alt,
+            target.lat,
+            target.lon,
+            target.alt,
         )
         if self.n_attacks_left <= 0:
             raise OutOfAttacksException("No attack left.")
         if d > self.combat_range:
             raise OutOfRangeException(
-                f"Target at {track_point} out of range ({self.combat_range:.1f} m)."
+                f"Target at {target.point} out of range ({self.combat_range:.1f} m)."
             )
-        if not self.terrain.has_line_of_sight(self.point, track_point):
-            raise NoLosException(f"No line-of-sight to target at {track_point}.")
+        if not self.terrain.has_line_of_sight(self.point, target.point):
+            raise NoLosException(f"No line-of-sight to target at {target.point}.")
 
         self.n_attacks_left -= 1
 
@@ -69,7 +63,7 @@ class DirectFireEffector(AbstractEffector):
             id=UNKNOWN_ID,
             time=UNKNOWN_TIME,
             shooter=self,
-            track=track,
+            target=target,
         )
 
 
@@ -77,24 +71,20 @@ class DirectFireEffector(AbstractEffector):
 class IndirectFireEffector(AbstractEffector):
     projectile: DirectFireEffector
 
-    def fire(self, track: Track) -> IndirectShot:
-        x, y, z, vx, vy, vz = track._y[-1]
-        lat, lon, alt = CoordinateTransformations.cartesian_to_geodetic(x, y, z)
-        track_point = Point(lat=lat, lon=lon, alt=alt)
-    
+    def fire(self, target: Target) -> IndirectShot:
         d = line_of_sight_distance(
             self.point.lat,
             self.point.lon,
             self.point.alt,
-            lat,
-            lon,
-            alt,
+            target.lat,
+            target.lon,
+            target.alt,
         )
         if self.n_attacks_left <= 0:
             raise OutOfAttacksException("No attack left.")
         if d > self.combat_range:
             raise OutOfRangeException(
-                f"Target at {track_point} out of range ({self.combat_range:.1f} m)."
+                f"Target at {target.point} out of range ({self.combat_range:.1f} m)."
             )
 
         self.n_attacks_left -= 1
@@ -103,6 +93,6 @@ class IndirectFireEffector(AbstractEffector):
             id=UNKNOWN_ID,
             time=UNKNOWN_TIME,
             shooter=self,
-            track=track,
+            target=target,
             projectile=self.projectile,
         )
