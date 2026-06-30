74840f4f4d07db228e32c64d05eedfae3c57a123
Renato Bellotti <184365954+RenatoBellottiCH@users.noreply.github.com>
2026-06-30 16:28:17 +0200

    Bug fixes (adapt server running to code changes)

--- examples/server_running.py
+++ examples/server_running.py
@@ -1,5 +1,6 @@
 import numpy as np
 import uvicorn
+from theia.simulation.damage_model import UniformDamageModel
 from theia.simulation.factories.bodensee_monostatic import BodenseeMonostaticFactory
 from theia.simulation.factories.performance_demo import PerformanceDemoFactory
 from theia.simulation.factories.uetliberg_opensky import (
@@ -18,10 +19,11 @@ print("Initialise...")
 rng = np.random.Generator(np.random.PCG64(seed=4054080))
 tree = HbvTree.load("tree_lat46:49_lon7:11_subsamplestride2.zip")
 terrain = FastSrtmModel(tree=tree, srtm_model=SrtmTerrainModel(), t_min=30)
+damage_model = UniformDamageModel(1.0, rng)  # Every shot kills.
 factory = PerformanceDemoFactory(rng, terrain)
 # factory = UetlibergOpenskySimulatorFactory()
 # factory = BodenseeMonostaticFactory(rng)
-simulator, buffer = factory.build_simulator(True, rng, terrain)
+simulator, buffer = factory.build_simulator(True, rng, terrain, damage_model)
 
 ################################################
 # Setup the server.
