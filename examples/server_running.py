93303283a5b274ac11b4b864d4449e2d05ac9889
Jürg Huber <>
2026-07-20 08:00:13 +0200

    Feature: Long-range attack on Zürich airport (demo) scenario

--- examples/server_running.py
+++ examples/server_running.py
@@ -8,6 +8,7 @@ from theia.simulation.factories.single_target_single_effector import SingleTarge
 from theia.simulation.factories.uetliberg_opensky import (
     UetlibergOpenskySimulatorFactory,
 )
+from theia.simulation.factories.zurich_airport import ZurichAirportScenarioFactory
 from theia.simulation.server import create_app
 from theia.simulation.simulation_director import SimulationDirector
 from theia.terrain import SrtmTerrainModel
@@ -20,9 +21,11 @@ print("Initialise...")
 ################################################
 rng = np.random.Generator(np.random.PCG64(seed=4054080))
 tree = HbvTree.load("tree_lat46:49_lon7:11_subsamplestride2.zip")
-terrain = FastSrtmModel(tree=tree, srtm_model=SrtmTerrainModel(), t_min=30)
+terrain = SrtmTerrainModel()
+# terrain = FastSrtmModel(tree=tree, srtm_model=SrtmTerrainModel(), t_min=30)
 damage_model = UniformDamageModel(1.0, rng)  # Every shot kills.
-factory = DavosDroneScenarioFactory(rng, terrain)
+factory = ZurichAirportScenarioFactory(rng, terrain)
+# factory = DavosDroneScenarioFactory(rng, terrain)
 # factory = SingleTargetSingleEffectorFactory(rng, terrain, True)
 # factory = PerformanceDemoFactory(rng, terrain)
 # factory = UetlibergOpenskySimulatorFactory(rng, terrain)
