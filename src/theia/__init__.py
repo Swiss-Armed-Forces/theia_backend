2cf015ef3ac53650e7861e89d73ba2275acec16e
Jürg Huber <>
2026-08-11 07:33:18 +0200

    Setup package to use git tags for versioning

--- src/theia/__init__.py
+++ src/theia/__init__.py
@@ -1,10 +1,13 @@
+from importlib.metadata import version
+
 from theia.terrain import DummyTerrain, SrtmTerrainModel
 from theia.terrain_fast_los import FastSrtmModel
 
-from .types import Point, AbstractSensor
-from .radar_equation import calculate_maximum_monostatic_range
-from .distance import haversine, linspace
 from .coverage import calculate_coverage
+from .distance import haversine, linspace
+from .radar_equation import calculate_maximum_monostatic_range
+from .types import AbstractSensor, Point
 
+__version__ = version("theia")
 
 TerrainModel = SrtmTerrainModel | FastSrtmModel | DummyTerrain
