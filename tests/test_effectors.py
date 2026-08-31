1f8b1748cfd9835737a45c34af569ff18334547a
Jürg Huber <>
2026-08-31 16:04:11 +0200

    Code cleanup: Remove indirect fire controller

--- tests/test_effectors.py
+++ tests/test_effectors.py
@@ -1,13 +1,9 @@
-import datetime
 import unittest
 
-import numpy as np
-
 from theia.config import SIDC, UNKNOWN_ID, UNKNOWN_TIME
-from theia.coordinates import POSITIONS_OF_INTEREST, CoordinateTransformations
+from theia.coordinates import POSITIONS_OF_INTEREST
 from theia.effectors import (
     DirectFireEffector,
-    IndirectFireEffector,
     NoLosException,
     OutOfAttacksException,
     OutOfRangeException,
@@ -16,10 +12,8 @@ from theia.terrain import SrtmTerrainModel
 from theia.types import (
     ConstantRcsModel,
     DirectShot,
-    IndirectShot,
     Point,
     Target,
-    Track,
     Velocity,
 )
 
@@ -167,131 +161,6 @@ class DirectEffectorTest(unittest.TestCase):
         self.assertEqual(event.shooter, effector)
         self.assertEqual(event.id, UNKNOWN_ID)
         self.assertEqual(event.time, UNKNOWN_TIME)
-
-
-class IndirectEffectorTest(unittest.TestCase):
-    def setUp(self):
-        self._projectile = DirectFireEffector(
-            id=0,
-            point=p_uetliberg,
-            combat_range=100_000,
-            n_attacks_left=1,
-            name="",
-            terrain=srtm,
-        )
-
-    def test_too_far_away(self):
-        effector = IndirectFireEffector(
-            id=0,
-            point=p_uetliberg,
-            combat_range=10_000,
-            n_attacks_left=1,
-            name="",
-            projectile=self._projectile,
-        )
-        target = Target(
-            id=0,
-            is_stationary=False,
-            sidc=SIDC.UNKNOWN,
-            point=p_bern,
-            cross_section_model=ConstantRcsModel(rcs=1.0),
-            velocity=Velocity(vx=0.0, vy=0.0, vz=0.0),
-        )
-
-        with self.assertRaises(OutOfRangeException):
-            effector.fire(target)
-
-    def test_no_attacks_left(self):
-        effector = IndirectFireEffector(
-            id=0,
-            point=p_uetliberg,
-            combat_range=10_000,
-            n_attacks_left=0,
-            name="",
-            projectile=self._projectile,
-        )
-        target = Target(
-            id=0,
-            is_stationary=False,
-            sidc=SIDC.UNKNOWN,
-            point=p_close,
-            cross_section_model=ConstantRcsModel(rcs=1.0),
-            velocity=Velocity(vx=0.0, vy=0.0, vz=0.0),
-        )
-
-        with self.assertRaises(OutOfAttacksException):
-            effector.fire(target)
-
-    def test_los_irrelevant(self):
-        effector = IndirectFireEffector(
-            id=0,
-            point=p_uetliberg,
-            combat_range=100_000,
-            n_attacks_left=1,
-            name="",
-            projectile=self._projectile,
-        )
-        target = Target(
-            id=0,
-            is_stationary=False,
-            sidc=SIDC.UNKNOWN,
-            point=p_no_los,
-            cross_section_model=ConstantRcsModel(rcs=1.0),
-            velocity=Velocity(vx=0.0, vy=0.0, vz=0.0),
-        )
-
-        effector.fire(target)
-
-    def test_use_ammo(self):
-        effector = IndirectFireEffector(
-            id=0,
-            point=p_uetliberg,
-            combat_range=100_000,
-            n_attacks_left=1,
-            name="",
-            projectile=self._projectile,
-        )
-        target = Target(
-            id=0,
-            is_stationary=False,
-            sidc=SIDC.UNKNOWN,
-            point=p_close,
-            cross_section_model=ConstantRcsModel(rcs=1.0),
-            velocity=Velocity(vx=0.0, vy=0.0, vz=0.0),
-        )
-
-        self.assertEqual(effector.n_attacks_left, 1)
-        effector.fire(target)
-        self.assertEqual(effector.n_attacks_left, 0)
-
-        with self.assertRaises(OutOfAttacksException):
-            effector.fire(target)
-
-    def test_event(self):
-        effector = IndirectFireEffector(
-            id=0,
-            point=p_uetliberg,
-            combat_range=100_000,
-            n_attacks_left=1,
-            name="",
-            projectile=self._projectile,
-        )
-        target = Target(
-            id=0,
-            is_stationary=False,
-            sidc=SIDC.UNKNOWN,
-            point=p_close,
-            cross_section_model=ConstantRcsModel(rcs=1.0),
-            velocity=Velocity(vx=0.0, vy=0.0, vz=0.0),
-        )
-
-        event = effector.fire(target)
-        self.assertIsInstance(event, IndirectShot)
-        self.assertEqual(event.target, target)
-        self.assertEqual(event.shooter, effector)
-        self.assertEqual(event.id, UNKNOWN_ID)
-        self.assertEqual(event.time, UNKNOWN_TIME)
-        self.assertEqual(event.projectile, self._projectile)
 
 
 if __name__ == "__main__":
