Swiss-Armed-Forces/theia_backend

commit b3974a5697aefd594667efa17d6f243400d11e56

Bug fixes

src/theia/snr.py
@@ -1,3 +1,5 @@
+import math
+
 import numba
 import numpy as np
 import scipy.constants as sc
@@ -162,6 +164,8 @@ def calculate_antenna_pattern_gain(
         p_observer=p,
         p_target=point_of_interest,
     )
+    theta_bearing = (theta_bearing + 2 * math.pi) % 2 * math.pi
+    theta_vert = (theta_vert + 2 * math.pi) % 2 * math.pi
 
     horiz_att = (
         transmitter_or_receiver.horizontal_attenuation(theta_bearing)

src/theia/simulation/controllers/geojson_controller.py
@@ -15,7 +15,7 @@ class GeoJsonController(Controller, pydantic.BaseModel):
     """wrapper that adds GeoJSON to any controller"""
 
     child: Controller
-    geojson_features: dict[str, GeoJSONFeature]
+    geojson_features: dict[str, list[GeoJSONFeature]]
 
     def model_post_init(self, context):
         super().model_post_init(context)

src/theia/types.py
@@ -1329,7 +1329,7 @@ class Controller(
     pet_receivers: list[Receiver] = []
     visual_sensors: list[VisualSensor] = []
     firing_effectors: list[tuple[AbstractEffector, Point]] = []
-    geojson: dict[str, GeoJSONFeature] = {}
+    geojson: dict[str, list[GeoJSONFeature]] = {}
 
     @abc.abstractmethod
     def update(

commit 71a3b8929b5156a6bf714eba00cdaa63812a92d9

Bug fix: Missing parentheses Introduced in b3974a5697aefd594667efa17d6f243400d11e56

src/theia/snr.py
@@ -164,8 +164,8 @@ def calculate_antenna_pattern_gain(
         p_observer=p,
         p_target=point_of_interest,
     )
-    theta_bearing = (theta_bearing + 2 * math.pi) % 2 * math.pi
-    theta_vert = (theta_vert + 2 * math.pi) % 2 * math.pi
+    theta_bearing = (theta_bearing + 2 * math.pi) % (2 * math.pi)
+    theta_vert = (theta_vert + 2 * math.pi) % (2 * math.pi)
 
     horiz_att = (
         transmitter_or_receiver.horizontal_attenuation(theta_bearing)
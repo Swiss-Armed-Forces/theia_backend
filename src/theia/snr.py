71a3b8929b5156a6bf714eba00cdaa63812a92d9
Renato Bellotti <184365954+RenatoBellottiCH@users.noreply.github.com>
2026-09-18 08:43:00 +0200

    Bug fix: Missing parentheses Introduced in b3974a5697aefd594667efa17d6f243400d11e56

--- src/theia/snr.py
+++ src/theia/snr.py
@@ -164,8 +164,8 @@ def calculate_antenna_pattern_gain(
         p_observer=p,
         p_target=point_of_interest,
     )
-    theta_bearing = (theta_bearing + 2 * math.pi) % 2 * math.pi
-    theta_vert = (theta_vert + 2 * math.pi) % 2 * math.pi
+    theta_bearing = (theta_bearing + 2 * math.pi) % (2 * math.pi)
+    theta_vert = (theta_vert + 2 * math.pi) % (2 * math.pi)
 
     horiz_att = (
         transmitter_or_receiver.horizontal_attenuation(theta_bearing)
